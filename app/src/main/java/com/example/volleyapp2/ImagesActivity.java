2a72b38cb26eb852c5c3b50c96bea31d371dd307
Ilia Maltsev <maltsev.ilia@outlook.com>
2015-05-08 16:31:43 +0700

    WsyoOK

--- app/src/main/java/com/example/volleyapp2/ImagesActivity.java
+++ app/src/main/java/com/example/volleyapp2/ImagesActivity.java
@@ -64,62 +64,6 @@ public class ImagesActivity extends ActionBarActivity {
      * A placeholder fragment containing a simple view.
      */
     public static class ImagesFragment extends Fragment {
-        private ArticleAdapter mAdapter;
 
-
-        public ImagesFragment() {
-        }
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View view	= inflater.inflate(R.layout.fragment_images, container, false);
-            ListView listView=(ListView)view.findViewById(R.id.list1);
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                public void onItemClick(AdapterView<?> parent, View view,
-                                       int position, long id) {
-
-                    Intent i = new Intent(getActivity(), PageArt.class);
-                    i.putExtra("position", position);
-                    startActivity(i);
-                }
-            });
-                return inflater.inflate(R.layout.fragment_images,container,false);
-            }
-            @Override
-        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-            super.onActivityCreated(savedInstanceState);
-            mAdapter = new ArticleAdapter(getActivity());
-            ListView listView = (ListView) getView().findViewById(R.id.list1);
-            listView.setAdapter(mAdapter);
-            fetch();
-        }
-        private void fetch() {
-            StringRequest request = new StringRequest(Request.Method.GET, "http://live.goodline.info/guest",
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String data) {
-                            Document doc = Jsoup.parse(data);
-                            List<Article> Articles=new ArrayList<>();
-
-                                Elements metaElems =doc.getElementsByTag("article");
-                                for(Element thisArt :metaElems) {
-                                    String title=thisArt.select("h2").text();
-                                    String url=thisArt.select("img").attr("src");
-                                    String dt=thisArt.select("time").text();
-                                    Article art=new Article(url,title,dt);
-                                    Articles.add(art);
-                                }
-                                mAdapter.swapArticleRecords(Articles);
-                            }
-                    },
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError volleyError) {
-                            // Handle error
-                        }
-                    }
-            );
-            VolleyApplication.getInstance().getRequestQueue().add(request);
-        }
     }
 }
---
+++ app/src/main/java/com/example/volleyapp2/PopitkaFragment.java
@@ -0,0 +1,85 @@
+package com.example.volleyapp2;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PopitkaFragment extends Fragment {
+    private ArticleAdapter mAdapter;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view	= inflater.inflate(R.layout.fragment_popitka, container, false);
+        ListView listView=(ListView)view.findViewById(R.id.list1);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                Intent i = new Intent(getActivity(), PageArt.class);
+                i.putExtra("position", position);
+                startActivity(i);
+
+            }
+        });
+        return view;
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mAdapter = new ArticleAdapter(getActivity());
+        ListView listView = (ListView) getView().findViewById(R.id.list1);
+        listView.setAdapter(mAdapter);
+        fetch();
+    }
+    private void fetch() {
+        StringRequest request = new StringRequest(Request.Method.GET, "http://live.goodline.info/guest",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String data) {
+                        Document doc = Jsoup.parse(data);
+                        List<Article> Articles=new ArrayList<>();
+
+                        Elements metaElems =doc.getElementsByTag("article");
+                        for(Element thisArt :metaElems) {
+                            String title=thisArt.select("h2").text();
+                            String url=thisArt.select("img").attr("src");
+                            String dt=thisArt.select("time").text();
+                            Article art=new Article(url,title,dt);
+                            Articles.add(art);
+                        }
+                        mAdapter.swapArticleRecords(Articles);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        // Handle error
+                    }
+                }
+        );
+        VolleyApplication.getInstance().getRequestQueue().add(request);
+    }
+}
